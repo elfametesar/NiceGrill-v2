bdc2ed8623634e27b4e86662e347cce490a7c5c3
Elfa Metesar <elfametesar@gmail.com>
2023-10-01 12:51:54 +0000

    No message

--- database/redisdb/redis.py
+++
@@ -1,7 +0,0 @@
-from redis.asyncio import Redis
-from redis import Redis as NonAsyncRedis
-from config import REDIS_CREDS
-
-
-AsyncRedis = Redis(**REDIS_CREDS, decode_responses=True)
-SyncRedis = NonAsyncRedis(**REDIS_CREDS, decode_responses=True)
