95fdf1f92a5b29703a019220933c6c096850c2ac
elfametesar <elfametesar@gmail.com>
2023-10-08 15:57:27 -0400

    No message

--- nicegrill/modules/quote.py
+++ nicegrill/modules/quote.py
@@ -714,7 +714,8 @@ class Quote:
             if char == "\n" or x + font.getlength(char) > Quote.MAXIMUM_BOX_WIDTH:
                 x = x_offset
                 y += Quote.LINE_HEIGHT
-                continue
+                if char == "\n":
+                    continue
 
             if text_image.width < x + font.getlength(char) < Quote.MAXIMUM_BOX_WIDTH:
                 additional_section = Image.new(
