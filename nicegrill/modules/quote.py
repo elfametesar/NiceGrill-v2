4b92dc0873ef668eedba5d83d35b0e1b08e726ae
elfametesar <elfametesar@gmail.com>
2023-10-09 15:30:09 +0000

    No message

--- nicegrill/modules/quote.py
+++ nicegrill/modules/quote.py
@@ -232,7 +232,7 @@ class Quote:
         thumb_file = None
         x_pos = 10
         
-        text = "    ".join(message.raw_text.splitlines()) if message.raw_text else ""
+        text = message.raw_text.replace("\n", "    ") if message.raw_text else ""
 
         if message.photo:
             text = text or "Photo"
